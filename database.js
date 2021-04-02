525cb7f524f27b86c604d753069e28a065277392
Jephtah Uche <jephtino@gmail.com>
2021-04-02 16:38:17 +0100

    docker build pipeline

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize( process.env.DB_SCHEMA || 'postgres',
-                                 process.env.DB_USER || 'jeph',
+                                 process.env.DB_USER || 'postgres',
                                  process.env.DB_PASSWORD || '',
                                 {
                                     host: process.env.DB_HOST || 'localhost',
