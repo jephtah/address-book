fde1aefbfc6574e6955f0ae40d24d311a4c07c70
Jephtah Uche <jephtino@gmail.com>
2021-04-02 18:21:28 +0100

    kubernetes deploy pipeline

--- database.js
+++ database.js
@@ -8,7 +8,10 @@ const sequelize = new Sequelize( process.env.DB_SCHEMA || 'postgres',
                                     port: process.env.DB_PORT || 5432,
                                     dialect: 'postgres',
                                     dialectOptions: {
-                                        ssl: process.env.DB_SSL === "true",
+                                      ssl: {
+                                          require: true,
+                                          rejectUnauthorized: false,
+                                      }
                                     }
 
                                 });
